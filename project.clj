95dcf5f0128d022ce896c9b46aee0a7d99635b8e
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-02-07 20:02:18 +0900

    first commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject jank-nrepl-server "0.1-SNAPSHOT"
+  :license {:name "MPL 2.0"
+            :url "https://www.mozilla.org/en-US/MPL/2.0/"}
+  :dependencies []
+  :plugins [[org.jank-lang/lein-jank "0.3"]]
+  :middleware [leiningen.jank/middleware]
+  :main jank-nrepl-server.main
+  :profiles {:debug {:jank {:optimization-level 0}}
+             :release {:jank {:optimization-level 2}}})
