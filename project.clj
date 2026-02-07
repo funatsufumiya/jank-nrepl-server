233452f35533273262ab6e833a6db222b2f9fde2
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-02-07 20:07:19 +0900

    add jank settings

--- project.clj
+++ project.clj
@@ -4,6 +4,9 @@
   :dependencies []
   :plugins [[org.jank-lang/lein-jank "0.3"]]
   :middleware [leiningen.jank/middleware]
+  :jank {:include-dirs ["cpp"]
+         :library-dirs ["cpp"]
+         :linked-libraries []}
   :main jank-nrepl-server.main
   :profiles {:debug {:jank {:optimization-level 0}}
              :release {:jank {:optimization-level 2}}})
