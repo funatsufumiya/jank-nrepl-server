b69d88fdbcce0634258ccf9e3d262c40deb596c9
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-02-15 22:04:32 +0900

    fix: branch msys2 or not

--- build-cpp.sh
+++ build-cpp.sh
@@ -6,5 +6,5 @@ cd cpp
 if [ -n "$MSYSTEM" ]; then
   clang++ -lws2_32 -shared -o libsimplesocket.dll -lz simple_socket_wrapper.cpp
 else
-  clang++ -lws2_32 -shared -o libsimplesocket.dll -lz simple_socket_wrapper.cpp
+  clang++ -shared -o libsimplesocket.dll -lz simple_socket_wrapper.cpp
 fi
