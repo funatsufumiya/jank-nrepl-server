673c51da151bcb5ef4dd6c05922893e6d2a1f9b3
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-02-15 22:14:19 +0900

    fix: build for mac

--- build-cpp.sh
+++ build-cpp.sh
@@ -5,6 +5,8 @@ cd cpp
 
 if [ -n "$MSYSTEM" ]; then
   clang++ -lws2_32 -shared -o libsimplesocket.dll -lz simple_socket_wrapper.cpp
+elif [ "$(uname)" == "Darwin" ]; then
+  clang++ -shared -o libsimplesocket.dylib -lz simple_socket_wrapper.cpp
 else
-  clang++ -shared -o libsimplesocket.dll -lz simple_socket_wrapper.cpp
+  clang++ -shared -o libsimplesocket.so -lz simple_socket_wrapper.cpp
 fi
