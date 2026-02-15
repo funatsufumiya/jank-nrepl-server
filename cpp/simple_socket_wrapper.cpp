3ae2a5d55b3917232bee16a5575821404f5a91fe
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-02-15 15:04:13 +0900

    feat: create wrapper interface

--- cpp/simple_socket_wrapper.cpp
+++ cpp/simple_socket_wrapper.cpp
@@ -8,11 +8,11 @@ namespace simplesocket {
 
 namespace kss = kani::simple_socket;
 
-class RecvMsg {
+class RecvMsgImpl : public RecvMsg {
 public:
     kss::RecvMsg msg;
 
-    RecvMsg(int length): msg(kss::RecvMsg(length)) {
+    RecvMsgImpl(int length): msg(kss::RecvMsg(length)) {
     }
 
     int recvLen(){
@@ -22,11 +22,20 @@ public:
     std::string_view get_msg(){
         return msg.m_msg;
     }
+
+    void* get_msg_impl(){
+        return &msg;
+    }
 };
 
-class SendMsg {
+class SendMsgImpl : public SendMsg {
 public:
     kss::SendMsg msg;
+
+    SendMsgImpl(){}
+    SendMsgImpl(std::string s){
+        msg.m_msg = s;
+    }
 
     int sentLen(){
         return msg.m_sentLen;
@@ -35,14 +44,18 @@ public:
     std::string_view get_msg(){
         return msg.m_msg;
     }
+
+    void* get_msg_impl(){
+        return &msg;
+    }
 };
 
-class TcpClient {
-public:
-    kani::simple_socket::TcpClient client;
-};
+// class TcpClientImpl : public TcpClient {
+// public:
+//     kani::simple_socket::TcpClient client;
+// };
 
-class TcpNetClient {
+class TcpNetClientImpl : public TcpNetClient {
 public:
     kani::simple_socket::TcpNetClient client;
 
@@ -57,11 +70,21 @@ public:
     void close(){
         client.close();
     }
+
+    // kani::simple_socket::TcpNetClient& get_client(){
+    //     return client;
+    // }
+
+    void* get_client_impl(){
+        return &client;
+    }
 };
 
-class TcpServer {
+class TcpServerImpl : public TcpServer {
 public:
     kani::simple_socket::TcpServer server;
+
+    TcpServerImpl(kss::TcpServerSocketInfo& info) : server(info) {}
 
     bool is_valid(){
         return server.is_valid();
@@ -72,23 +95,36 @@ public:
     }
 
     bool wait_client(TcpNetClient& client){
-        return server.wait_client(&client.client);
+        return server.wait_client(ptr_from(client));
     }
 
     bool send_msg(TcpNetClient& client, SendMsg& msg){
-        return server.send_msg(&client.client, &msg.msg);
+        return server.send_msg(ptr_from(client), msg_from(msg));
     }
 
     bool recv_msg(TcpNetClient& client, RecvMsg& msg){
-        return server.recv_msg(&client.client, &msg.msg);
+        return server.recv_msg(ptr_from(client), msg_from(msg));
     }
 
     void stop(){
         server.stop();
     }
+
+private:
+    kani::simple_socket::TcpNetClient* ptr_from(TcpNetClient& client){
+        return (kani::simple_socket::TcpNetClient*)(client.get_client_impl());
+    }
+
+    kani::simple_socket::SendMsg* msg_from(SendMsg& client){
+        return (kani::simple_socket::SendMsg*)(client.get_msg_impl());
+    }
+
+    kani::simple_socket::RecvMsg* msg_from(RecvMsg& client){
+        return (kani::simple_socket::RecvMsg*)(client.get_msg_impl());
+    }
 };
 
-TcpServer create_tcp_server(std::string host, int port){
+std::shared_ptr<TcpServer> create_tcp_server(std::string host, int port){
     // namespace kss = kani::simple_socket;
 
     std::string port_s = std::to_string(port);
@@ -100,7 +136,23 @@ TcpServer create_tcp_server(std::string host, int port){
     info.m_backlog = 0x7fffffff;
 
     // kss::TcpServer server(info);
-    return TcpServer{ kss::TcpServer(info) };
+    return std::make_shared<TcpServerImpl>(info);
+}
+
+std::shared_ptr<SendMsg> create_send_msg(){
+    return std::make_shared<SendMsgImpl>();
+}
+
+std::shared_ptr<SendMsg> create_send_msg(std::string s){
+    return std::make_shared<SendMsgImpl>(s);
+}
+
+std::shared_ptr<RecvMsg> create_recv_msg(int len){
+    return std::make_shared<RecvMsgImpl>(len);
+}
+
+std::shared_ptr<TcpNetClient> create_tcp_net_client(){
+    return std::make_shared<TcpNetClientImpl>();
 }
 
 int run_server() {
@@ -124,49 +176,48 @@ int run_server() {
 
     auto server = create_tcp_server("localhost", 1234);
 
-    if (!server.is_valid()) {
+    if (!server->is_valid()) {
         std::cerr << "can't initialized server!" << std::endl;
         return 1;
     }
 
-    if (!server.start()) {
+    if (!server->start()) {
         std::cerr << "can't start the server!" << std::endl;
         return 1;
     }
 
     // kss::TcpNetClient client;
-    TcpNetClient client;
+    auto client = create_tcp_net_client();
     std::cout << "waiting for client..." << std::endl;
 
-    while (!server.wait_client(client)) { }
+    while (!server->wait_client(*client)) { }
 
-    std::cout << "client ( " << client.get_host() << " : " << client.get_service() << " ) connected!" << std::endl;
+    std::cout << "client ( " << client->get_host() << " : " << client->get_service() << " ) connected!" << std::endl;
 
-    SendMsg msg;
-    msg.msg.m_msg = "Hello, client! I'm a server. Welcome to my simple server 8)";
+    auto msg = create_send_msg("Hello, client! I'm a server. Welcome to my simple server 8)");
 
-    if (!server.send_msg(client, msg)) {
+    if (!server->send_msg(*client, *msg)) {
         std::cerr << "can't sent message to client!" << std::endl;
         return 1;
     }
 
-    std::cout << "sent message to client: " << msg.sentLen() << "byte" << std::endl;
+    std::cout << "sent message to client: " << msg->sentLen() << "byte" << std::endl;
 
-    RecvMsg response(255);
+    auto response = create_recv_msg(255);
 
-    if (!server.recv_msg(client, response)) {
+    if (!server->recv_msg(*client, *response)) {
         std::cerr << "can't received message from client!" << std::endl;
         return 1;
     }
 
-    std::cout << "received message from client: " << response.recvLen() << "byte" << std::endl;
-    std::cout << "Client: " << response.get_msg() << std::endl;
+    std::cout << "received message from client: " << response->recvLen() << "byte" << std::endl;
+    std::cout << "Client: " << response->get_msg() << std::endl;
 
     std::cout << "Perfect! Now, say to goodbye :')" << std::endl;
-    client.close();
+    client->close();
 
     std::cout << "stopping the server..." << std::endl;
-    server.stop();
+    server->stop();
 
     std::cout << "server stopped" << std::endl;
     return 0;
--- cpp/simple_socket_wrapper.hpp
+++ cpp/simple_socket_wrapper.hpp
@@ -1,12 +1,56 @@
+#include <string>
+#include <memory>
+
 namespace simplesocket {
 
-class TcpServer;
-class TcpClient;
-class TcpNetClient;
+class RecvMsg {
+public:
+    // RecvMsg(int len){}
+    virtual ~RecvMsg(){}
+    virtual int recvLen() = 0;
+    virtual std::string_view get_msg() = 0;
+    virtual void* get_msg_impl() = 0;
+};
 
-TcpServer create_tcp_server(std::string host, int port);
-bool is_valid(TcpServer& server);
-bool start(TcpServer& server);
+class SendMsg {
+public:
+    // virtual SendMsg();
+    // virtual SendMsg(std::string s);
+    virtual ~SendMsg(){}
+    virtual int sentLen() = 0;
+    virtual std::string_view get_msg() = 0;
+    virtual void* get_msg_impl() = 0;
+};
+
+// class TcpClient {
+// public:
+// };
+
+class TcpNetClient {
+public:
+    // TcpNetClient(){}
+    virtual ~TcpNetClient(){}
+    virtual std::string_view get_host() = 0;
+    virtual std::string_view get_service() = 0;
+    virtual void close() = 0;
+    virtual void* get_client_impl() = 0;
+};
+
+class TcpServer {
+public:
+    // TcpServer(){}
+    virtual ~TcpServer(){}
+    virtual bool is_valid() = 0;
+    virtual bool start() = 0;
+    virtual bool wait_client(TcpNetClient& client) = 0;
+    virtual bool send_msg(TcpNetClient& client, SendMsg& msg) = 0;
+    virtual bool recv_msg(TcpNetClient& client, RecvMsg& msg) = 0;
+    virtual void stop() = 0;
+};
+
+std::shared_ptr<TcpServer> create_tcp_server(std::string host, int port);
+std::shared_ptr<SendMsg> create_send_msg();
+std::shared_ptr<RecvMsg> create_recv_msg();
 
 int run_server();
 
