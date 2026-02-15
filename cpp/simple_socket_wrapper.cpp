1ffcae703d3de668173bcfae37be13af03e7fad1
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-02-15 13:40:29 +0900

    feat: create some base classes

--- cpp/simple_socket_wrapper.cpp
+++ cpp/simple_socket_wrapper.cpp
@@ -5,41 +5,57 @@
 
 namespace simplesocket {
 
+class TcpServer {
+public:
+    kani::simple_socket::TcpServer server;
+};
+
+class TcpClient {
+public:
+    kani::simple_socket::TcpClient client;
+};
+
+class TcpNetClient {
+public:
+    kani::simple_socket::TcpNetClient net_client;
+};
+
 int run_server() {
-    using namespace kani::simple_socket;
+    // using namespace kani::simple_socket;
+    namespace kss = kani::simple_socket;
 
     const std::string DEFAULT_IP = "localhost";
     const std::string DEFAULT_PORT = "1234";
-    const auto DEFAULT_PROTOCOL_FAMILY = AF_UNSPEC;
-    const auto DEFAULT_BACKLOG = SOMAXCONN;
+    const int DEFAULT_PROTOCOL_FAMILY = 0;
+    const long DEFAULT_BACKLOG = 0x7fffffff;
     const int DEFAULT_RECV_MSG_LEN = 255;
 
-    TcpServerSocketInfo info;
+    kss::TcpServerSocketInfo info;
     info.m_node                 = DEFAULT_IP;
     info.m_service             = DEFAULT_PORT;
     info.m_protocolFamily = DEFAULT_PROTOCOL_FAMILY;
     info.m_backlog            = DEFAULT_BACKLOG;
 
-    TcpServer server(info);
+    kss::TcpServer server(info);
 
     if (!server.is_valid()) {
         std::cerr << "can't initialized server!" << std::endl;
         return 1;
     }
 
-    if (server.start() != SS_START_RESULT_SUCCESS) {
+    if (server.start() != kss::SS_START_RESULT_SUCCESS) {
         std::cerr << "can't start the server!" << std::endl;
         return 1;
     }
 
-    TcpNetClient client;
+    kss::TcpNetClient client;
     std::cout << "waiting for client..." << std::endl;
 
     while (!server.wait_client(&client)) { }
 
     std::cout << "client ( " << client.get_host() << " : " << client.get_service() << " ) connected!" << std::endl;
 
-    SendMsg msg;
+    kss::SendMsg msg;
     msg.m_msg = "Hello, client! I'm a server. Welcome to my simple server 8)";
 
     if (!server.send_msg(&client, &msg)) {
@@ -49,7 +65,7 @@ int run_server() {
 
     std::cout << "sent message to client: " << msg.m_sentLen << "byte" << std::endl;
 
-    RecvMsg response(DEFAULT_RECV_MSG_LEN);
+    kss::RecvMsg response(DEFAULT_RECV_MSG_LEN);
 
     if (!server.recv_msg(&client, &response)) {
         std::cerr << "can't received message from client!" << std::endl;
--- cpp/simple_socket_wrapper.hpp
+++ cpp/simple_socket_wrapper.hpp
@@ -1,5 +1,9 @@
 namespace simplesocket {
 
+class TcpServer;
+class TcpClient;
+class TcpNetClient;
+
 int run_server();
 
 } // namespace simplesocket
